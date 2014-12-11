19719b74dbe86ab9070adebc58b5673e49009dc8
André Willik Valenti <awvalenti@gmail.com>
2014-12-11 15:59:52 -0200

    Projeto clichê para VRaptor 3. Falta testar JPA.

---
+++ java/web/projeto-cliche-web-vraptor3/src/main/java/br/minhaempresa/meuprojeto/web/controllers/MinhaEntidadeController.java
@@ -0,0 +1,20 @@
+package br.minhaempresa.meuprojeto.web.controllers;
+
+import static java.util.Arrays.asList;
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+
+@Resource
+public class MinhaEntidadeController {
+
+	private final Result result;
+
+	public MinhaEntidadeController(Result result) {
+		this.result = result;
+	}
+
+	public void ola() {
+		result.include("mensagens", asList("Ola", "mundo!"));
+	}
+
+}
